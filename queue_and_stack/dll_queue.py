c899c4a85ea33c604f6091564812f71ffbc31b2f
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-12-16 16:10:41 -0700

    MVP day 1

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,21 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # Increase the size counter by 1
+        self.size += 1
+        # Add item to the tail
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        if self.size < 1:
+            return None
+        # Decrease the size of the queue by 1
+        self.size -= 1
+        # Call method to remove item from the head
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return int(self.size)
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,13 +6,21 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # Increase the size counter by 1
+        self.size += 1
+        # Add item to the head
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        if self.size < 1:
+            return None
+        # Decrease the size of the queue by 1
+        self.size -= 1
+        # Call method to remove item from the head
+        return self.storage.remove_from_head()
 
     def len(self):
-        pass
+        return int(self.size)
